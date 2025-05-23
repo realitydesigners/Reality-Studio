efbe8347448c8cc62a0cb3a7f1b3c6b93d6d128e
Raymond <raymond.luke.spartz@gmail.com>
2025-05-23 13:54:14 -0500

    init

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (
+    <div className="bg-black w-screen h-screen">
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    inlineCss: true,
+    
+  },
+};
+
+export default nextConfig;
